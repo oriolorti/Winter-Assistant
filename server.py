c0639fc54b569bff921a0794a0f14cc872492cf6
oriolorti <169072249+oriolorti@users.noreply.github.com>
2026-03-11 12:47:33 +0100

    Update server.py

--- server.py
+++ server.py
@@ -32,8 +32,3 @@ def search():
     response = requests.post(url, headers=headers, json=data)
 
     return jsonify(response.json())
-
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
